f45f786977cdbb8b55e68955a64600aa14f2ab91
Luiza Bortolotti de Carvalho <bortolottiluiza@gmail.com>
2024-06-04 12:34:56 -0300

    Atualização da mensagem de erro.

--- conta-bancaria/src/VerificacaoContaBancaria.java
+++ conta-bancaria/src/VerificacaoContaBancaria.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class VerificacaoContaBancaria {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in); 
-        //System.out.println("Informe o seu número de conta:");
+        System.out.println("Informe o seu número de conta:");
         String numeroConta = scanner.nextLine();
 
         try {
             verificarConta(numeroConta);
         } catch (IllegalArgumentException e) {
-            System.out.println("Erro: Numero de conta invalido. Digite exatamente 8 digitos.");
-        }
-        finally {
+            System.out.println("Erro: " + e.getMessage());
+        } finally {
             scanner.close();
         }
     }
@@ -20,7 +19,7 @@ public class VerificacaoContaBancaria {
         if(numeroConta.length() == 8){
             System.out.println("Numero de conta valido.");
         } else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Numero de conta invalido. Digite exatamente 8 digitos.");
         }
     }
 }
